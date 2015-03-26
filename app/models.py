f8da2e0eb8278e38eaab323e92f041fc57366ef3
William Burton <burton.wj@gmail.com>
2015-03-25 21:50:56 -0400

    updated 404 and 500 back buttons

--- app/models.py
+++ app/models.py
@@ -23,7 +23,6 @@ followers = db.Table(
 
 
 class User(db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
