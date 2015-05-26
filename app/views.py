c232756619159626b4528f102a62479425fb10be
William Burton <burton.wj@gmail.com>
2015-05-26 11:06:51 -0400

    resolved task 2, automatic switch for local url

--- app/views.py
+++ app/views.py
@@ -24,6 +24,8 @@ import json
 def inject_static_url():
     local_static_url = app.static_url_path
     static_url = 'https://s3.amazonaws.com/netbardus/'
+    if os.environ.get('HEROKU') is not None:
+        local_static_url = static_url
     if not static_url.endswith('/'):
         static_url += '/'
     if not local_static_url.endswith('/'):
