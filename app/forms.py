c5d96317392111bb2a185c950272f656d664207c
Bill Burton <bburton@ripplefly.com>
2015-06-06 17:29:36 -0400

    cleanup

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField, SubmitField, PasswordField, SelectField
-from flask.ext.wtf.file import FileField, FileRequired, FileAllowed
-from wtforms.validators import DataRequired, Email, Length, ValidationError
+from flask.ext.wtf.file import FileField, FileAllowed
+from wtforms.validators import DataRequired, Email, Length
 from .models import User
 
 
 class LoginForm(Form):
-    email = StringField("Email",  [DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    email = StringField("Email",  [DataRequired("Please enter your email address."),
+                                   Email("Please enter your email address.")])
     password = PasswordField('Password', [DataRequired("Please enter a password.")])
     remember_me = BooleanField('remember_me', default=False)
 
@@ -28,7 +29,8 @@ class LoginForm(Form):
 class SignupForm(Form):
     firstname = StringField("First name",  [DataRequired("Please enter your first name.")])
     lastname = StringField("Last name",  [DataRequired("Please enter your last name.")])
-    email = StringField("Email",  [DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    email = StringField("Email",  [DataRequired("Please enter your email address."),
+                                   Email("Please enter your email address.")])
     password = PasswordField('Password', [DataRequired("Please enter a password.")])
     submit = SubmitField("Create account")
     remember_me = BooleanField('remember_me', default=False)
@@ -36,22 +38,22 @@ class SignupForm(Form):
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
 
-        def validate(self):
-            if not Form.validate(self):
-                return False
+    def validate(self):
+        if not Form.validate(self):
+            return False
 
-            user = User.query.filter_by(email = self.email.data.lower()).first()
-            if user:
-                self.email.errors.append("That email is already taken")
-                return False
-            else:
-                return True
+        user = User.query.filter_by(email=self.email.data.lower()).first()
+        if user:
+            self.email.errors.append("That email is already taken")
+            return False
+        else:
+            return True
 
 
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
-    profile_photo = FileField('Your photo', validators=[FileAllowed(['jpg','png'], 'Images only!')])
+    profile_photo = FileField('Your photo', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
 
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
@@ -77,7 +79,7 @@ class PostForm(Form):
     post = TextAreaField('Post', validators=[DataRequired()])
     header = StringField('Header', validators=[DataRequired()])
     writing_type = SelectField('Post Type', choices=my_choices, default='poem')
-    photo = FileField('Your photo', validators=[FileAllowed(['jpg','png'], 'Images only!')])
+    photo = FileField('Your photo', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
     submit = SubmitField("Send")
 
 
