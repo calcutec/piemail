7edd9ca5e0c0965a4747892b02e293754c6a34e7
William Burton <burton.wj@gmail.com>
2015-10-27 13:06:50 -0400

    new key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = os.environ['FLASK_SECRET_KEY']
+SECRET_KEY = "For my eyes only"
 
 
 SQLALCHEMY_DATABASE_URI = os.environ['PIEMAIL_DATABASE_URL']
