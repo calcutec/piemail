aa6a95d7dc13cbb124ac9e71ec544376a6a8e4a1
William Burton <burton.wj@gmail.com>
2015-10-27 13:34:53 -0400

    remove database reference

--- config.py
+++ config.py
@@ -1,35 +1,12 @@
 # -*- coding: utf8 -*-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-CSRF_ENABLED = True
 SECRET_KEY = "For my eyes only"
 
 
-SQLALCHEMY_DATABASE_URI = os.environ['PIEMAIL_DATABASE_URL']
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-SQLALCHEMY_RECORD_QUERIES = True
-
-WHOOSH_BASE = os.path.join(basedir, 'search.db')
-# Whoosh does not work on Heroku
-WHOOSH_ENABLED = os.environ.get('HEROKU') is None
-
-# slow database query threshold (in seconds)
-DATABASE_QUERY_TIMEOUT = 0.5
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-# email server
-MAIL_SERVER = 'smtp.googlemail.com'  # your mailserver
-MAIL_PORT = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+# SQLALCHEMY_DATABASE_URI = os.environ['PIEMAIL_DATABASE_URL']
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLALCHEMY_RECORD_QUERIES = True
 
 # administrator list
 ADMINS = ['burton.wj@gmail.com']
-
-# pagination
-POSTS_PER_PAGE = 500
-MAX_SEARCH_RESULTS = 50
