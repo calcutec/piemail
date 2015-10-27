0e14370dfb5e406faac6a75dc8b65c9e9ebd524d
William Burton <burton.wj@gmail.com>
2015-10-27 13:40:46 -0400

    removed aws access key references

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 
-app.config["S3_LOCATION"] = 'https://s3.amazonaws.com/netbardus/'
-app.config["S3_UPLOAD_DIRECTORY"] = 'user_imgs'
-app.config["S3_BUCKET"] = 'netbardus'
-app.config["AWS_ACCESS_KEY_ID"] = os.environ['AWS_ACCESS_KEY_ID']
-app.config["AWS_SECRET_ACCESS_KEY"] = os.environ['AWS_SECRET_ACCESS_KEY']
+# app.config["S3_LOCATION"] = 'https://s3.amazonaws.com/netbardus/'
+# app.config["S3_UPLOAD_DIRECTORY"] = 'user_imgs'
+# app.config["S3_BUCKET"] = 'netbardus'
+# app.config["AWS_ACCESS_KEY_ID"] = os.environ['AWS_ACCESS_KEY_ID']
+# app.config["AWS_SECRET_ACCESS_KEY"] = os.environ['AWS_SECRET_ACCESS_KEY']
 
 from app import views
--- config.py
+++ config.py
@@ -7,6 +7,5 @@ SECRET_KEY = "For my eyes only"
 # SQLALCHEMY_DATABASE_URI = os.environ['PIEMAIL_DATABASE_URL']
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 # SQLALCHEMY_RECORD_QUERIES = True
-
 # administrator list
 ADMINS = ['burton.wj@gmail.com']
