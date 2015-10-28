647db0cc163a2c865163225873b673894db784a4
William Burton <burton.wj@gmail.com>
2015-10-28 15:56:31 -0400

    added view error handlers

--- app/views.py
+++ app/views.py
@@ -30,3 +30,12 @@ def emaildata(emailid):
     return render_template('emaildata.html', emailid=emailid)
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', error=error), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html', error=error), 500
