b8297467fe5d068b5fb788a145d1e6133a12a680
William Burton <burton.wj@gmail.com>
2015-10-29 16:26:20 -0400

    new branch with python connection

--- app/views.py
+++ app/views.py
@@ -1,11 +1,84 @@
-from app import app
-from flask import render_template, request
+from flask import url_for, session, redirect, request, json, render_template
+import httplib2
 import os
+
+from apiclient import discovery, errors
+from oauth2client import client
+
+
+from app import app
 
 
 @app.route('/')
 def index():
-    return render_template('piemail.html')
+    if 'credentials' not in session:
+        return redirect(url_for('oauth2callback'))
+    credentials = client.OAuth2Credentials.from_json(session['credentials'])
+    if credentials.access_token_expired:
+        return redirect(url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
+        gmail_service = discovery.build('gmail', 'v1', http_auth)
+        threads = gmail_service.users().threads().list(userId='me').execute()
+        return json.dumps(threads)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    flow = client.flow_from_clientsecrets(
+        'client_secrets.json',
+        scope='https://mail.google.com/',
+        redirect_uri=url_for('oauth2callback', _external=True)
+    )
+    if 'code' not in request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return redirect(auth_uri)
+    else:
+        auth_code = request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        session['credentials'] = credentials.to_json()
+        return redirect(url_for('index'))
+
+
+@app.route('/getmail')
+def getmail():
+    if 'credentials' not in session:
+        return redirect(url_for('oauth2callback'))
+    credentials = client.OAuth2Credentials.from_json(session['credentials'])
+    if credentials.access_token_expired:
+        return redirect(url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
+        gmail_service = discovery.build('gmail', 'v1', http_auth)
+        query = 'is:inbox'
+        """List all Messages of the user's mailbox matching the query.
+
+        Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        query: String used to filter messages returned.
+        Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+        Returns:
+        List of Messages that match the criteria of the query. Note that the
+        returned list contains Message IDs, you must use get with the
+        appropriate ID to get the details of a Message.
+        """
+        try:
+            response = gmail_service.users().messages().list(userId='me', q=query).execute()
+            messages = []
+            if 'messages' in response:
+                print 'test %s' % response
+                messages.extend(response['messages'])
+            while 'nextPageToken' in response:
+                page_token = response['nextPageToken']
+                response = gmail_service.users().messages().list(userId='me', q=query, pageToken=page_token).execute()
+                messages.extend(response['messages'])
+
+            return json.jsonify({'data': messages})
+        except errors.HttpError, error:
+            print 'An error occurred: %s' % error
 
 
 @app.context_processor
