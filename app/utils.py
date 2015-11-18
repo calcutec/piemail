c66b5a0ba7600110aef644291b0ed8d937532009
William Burton <burton.wj@gmail.com>
2015-11-18 16:17:43 -0500

    added utils folder

---
+++ app/utils.py
@@ -0,0 +1,88 @@
+import os
+from datetime import timedelta
+from flask import make_response, current_app, request, render_template
+from functools import update_wrapper
+from app import app
+from copy import deepcopy
+
+
+def getcachedthreads(parsedmessageset):
+    newcollection = None
+    cachedmessagesetids = cache.get('cachedmessagesetids')
+    if cachedmessagesetids:
+        for emailthreadid in cachedmessagesetids:
+            cachedthread = cache.get(emailthreadid['id'])
+            if cachedthread:
+                parsedmessageset.append(cachedthread)
+        newcollection = deepcopy(parsedmessageset)
+        parsedmessageset[:] = []
+    return newcollection
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
+@app.context_processor
+def inject_static_url():
+    local_static_url = app.static_url_path
+    static_url = 'https://s3.amazonaws.com/netbardus/'
+    if os.environ.get('HEROKU') is not None:
+        local_static_url = static_url
+    if not static_url.endswith('/'):
+        static_url += '/'
+    if not local_static_url.endswith('/'):
+        local_static_url += '/'
+    return dict(
+        static_url=static_url,
+        local_static_url=local_static_url,
+        host_url=request.url_root
+    )
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', error=error), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error), 500
